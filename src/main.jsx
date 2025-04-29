3858357fc8145d552806807c47c1bf6f6dea2caf
mohamed1868 <mohamedsayed20500@gmail.com>
2025-04-29 10:54:26 +0300

    add mail

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,36 @@
-import { StrictMode, useContext, useEffect } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { ConfigProvider } from 'antd';
-import {  DARK_ANT_THEME, LIGHT_ANT_THEME } from './Core/Config';
-import DarkModeContextComponent, { contextDarkMode } from './page/Context/DarkMode'
-import Home from './page/Public/Home/Home';
-import Aos from 'aos';
+import { StrictMode, useContext, useEffect } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { ConfigProvider } from "antd";
+import { DARK_ANT_THEME, LIGHT_ANT_THEME } from "./Core/Config";
+import DarkModeContextComponent, {
+  contextDarkMode
+} from "./page/Context/DarkMode";
+import Home from "./page/Public/Home/Home";
+import Aos from "aos";
 import "aos/dist/aos.css";
-
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
-  const {dark} = useContext(contextDarkMode)
-  useEffect(()=>{
+  const { dark } = useContext(contextDarkMode);
+  useEffect(() => {
     Aos.init({
-      duration:1500,
-      once: true, 
-    })
-  },[])
+      duration: 1500,
+      once: true
+    });
+  }, []);
   return (
-    <ConfigProvider theme={dark  ? LIGHT_ANT_THEME : DARK_ANT_THEME}>
+    <ConfigProvider theme={dark ? LIGHT_ANT_THEME : DARK_ANT_THEME}>
       <Home />
     </ConfigProvider>
   );
 };
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-      <DarkModeContextComponent>
-        <App />         
-      </DarkModeContextComponent>
+    <DarkModeContextComponent>
+      <Toaster position="top-center" reverseOrder={false} />
+      <App />
+    </DarkModeContextComponent>
   </StrictMode>
 );
