67ffbebbde8c378dff2cbbf19ac2a0ba9a0a9694
mohamed1868 <mohamedsayed20500@gmail.com>
2025-07-08 13:18:08 +0300

    update footer

--- src/page/Context/DarkMode.jsx
+++ src/page/Context/DarkMode.jsx
@@ -1,21 +1,19 @@
 import { createContext, useState } from "react";
 
-export const contextDarkMode = createContext(0)
-
+export const contextDarkMode = createContext(0);
 
 export default function DarkModeContextComponent(props) {
-   
-    const [dark , setDark] = useState(true)
-    const darkMode = ()=>{
-        setDark(false)
-    }
-    const lightMode = ()=>{
-        setDark(true)
-    }
+  const [dark, setDark] = useState(true);
+  const darkMode = () => {
+    setDark(false);
+  };
+  const lightMode = () => {
+    setDark(true);
+  };
 
-  return (<contextDarkMode.Provider value={{dark ,darkMode ,lightMode}}>
-     {props.children}
-  </contextDarkMode.Provider>
-    
-  )
+  return (
+    <contextDarkMode.Provider value={{ dark, darkMode, lightMode }}>
+      {props.children}
+    </contextDarkMode.Provider>
+  );
 }
--- src/page/Public/Home/Footer.jsx
+++ src/page/Public/Home/Footer.jsx
@@ -1,47 +1,61 @@
 import Link from "antd/es/typography/Link";
-import logoImg from "../../../Image/logo.webp"
-import logoImgDark from "../../../Image/Untitled-2.png-white.webp"
+import logoImg from "../../../Image/logo.webp";
+import logoImgDark from "../../../Image/Untitled-2.png-white.webp";
 import { useTranslation } from "react-i18next";
 import { useContext } from "react";
 import { contextDarkMode } from "../../Context/DarkMode";
+import { PhoneOutlined } from "@ant-design/icons";
 
 export default function Footer() {
-    const { t } = useTranslation()
-    const { dark } = useContext(contextDarkMode)
+  const { t } = useTranslation();
+  const { dark } = useContext(contextDarkMode);
 
-    return (<>
-        <div className="Footer">
-            <div style={{ marginTop: "30px", textAlign: "center" ,fontSize: "15px"}}>
-                {dark ?
-                    <img src={logoImg} alt="Logo" width={"100px"} height={"40px"} /> :
-                    <img src={logoImgDark} alt="Logo" width={"100px"} height={"40px"} />
-                }
-                <p style={{ marginTop:"10px"}}>  {t("FooterCol3Text1")} </p>
-                <p>info@invest-egypt.com</p>
-                <p >01283188882</p>
-            </div>
+  return (
+    <>
+      <div className="Footer">
+        <div
+          style={{ marginTop: "10px", textAlign: "center", fontSize: "19px" }}
+        >
+          <img src={logoImgDark} alt="Logo" width={"130px"} height={"60px"} />
 
-            <div style={{textAlign:"center"}}>
-                <h1>{t("FooterCol2Text1")} </h1>
-                <Link href="#SectionFour" style={{ fontSize: "15px" }}>
-                    <p style={{ marginTop:"10px"}}>{t("FooterCol2Text2")} </p>
-                </Link>
-                <Link href="#SectionSix" style={{ fontSize: "15px" }}>
-                    <p>{t("FooterCol2Text3")} </p>
-                </Link>
-                <Link href="#SectionSeven" style={{ fontSize: "15px" }}>
-                    <p>{t("FooterCol2Text4")} </p>
-                </Link>
-            </div>
-            <div style={{textAlign:"center",fontSize: "15px"}}>
-                <h1> {t("FooterCol1Text1")}</h1>
-                <p style={{ marginTop:"10px"}}>{t("FooterCol1Text2")}</p>
-                <p> {t("FooterCol1Text3")}</p>
-            </div>
+          <p style={{ marginTop: "10px" }}> {t("FooterCol3Text1")} </p>
+          <p style={{ marginTop: "10px" }}>info@invest-egypt.com</p>
+          <p style={{ marginTop: "10px" }}>
+            <PhoneOutlined style={{ padding: "0px 5px", fontSize: "22px" }} />{" "}
+            01283188882
+          </p>
         </div>
 
-        <div className="Footer" style={{ margin: "0px auto", fontSize: "15px" }}>
-            <p style={{ fontSize: "15px",textAlign:"center" }}> {t("FooterFinal")}</p>
+        <div style={{ textAlign: "center" }}>
+          <h1 style={{ fontSize: "20px", fontWeight: "bold" }}>
+            {t("FooterCol2Text1")}{" "}
+          </h1>
+          <Link href="#SectionFour" style={{ fontSize: "18px" }}>
+            <p style={{ marginTop: "15px" }}>{t("FooterCol2Text2")} </p>
+          </Link>
+          <Link href="#SectionSix" style={{ fontSize: "18px" }}>
+            <p>{t("FooterCol2Text3")} </p>
+          </Link>
+          <Link href="#SectionSeven" style={{ fontSize: "18px" }}>
+            <p>{t("FooterCol2Text4")} </p>
+          </Link>
         </div>
-    </>)
+        <div style={{ textAlign: "center", fontSize: "18px" }}>
+          <h1 style={{ fontSize: "20px", fontWeight: "bold" }}>
+            {" "}
+            {t("FooterCol1Text1")}
+          </h1>
+          <p style={{ marginTop: "15px" }}>{t("FooterCol1Text2")}</p>
+          <p style={{ marginTop: "10px" }}> {t("FooterCol1Text3")}</p>
+        </div>
+      </div>
+
+      <div className="Footer" style={{ margin: "0px auto", fontSize: "18px" }}>
+        <p style={{ fontSize: "18px", textAlign: "center" }}>
+          {" "}
+          {t("FooterFinal")}
+        </p>
+      </div>
+    </>
+  );
 }
